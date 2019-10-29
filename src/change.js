d94793d52202e8eeda2fda8242bc965ff7363246
mikeyshriner <shrinermichael@gmail.com>
2019-10-29 12:43:25 -0400

    initial commit

--- src/change.js
+++ src/change.js
@@ -3,11 +3,11 @@ const readlineSync = require("readline-sync");
 const MIN = 0.01;
 const MAX = 0.99;
 
-const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
+const Amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
-if (Number.isNaN(amount)) {
+if (Number.isNaN(Amount)) {
     // print your error message here
-} else if (amount < MIN || amount > MAX) {
+} else if (Amount < MIN || Amount > MAX) {
     // print your error message here
 } else {
     // write your code for making change here
