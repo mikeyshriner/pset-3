4f87276a50fcdfe68ddae65dafcaf2be94d13119
mikeyshriner <shrinermichael@gmail.com>
2019-11-04 12:37:16 -0500

    js files

--- src/change.js
+++ src/change.js
@@ -7,7 +7,6 @@ const Amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 Q = 0.25
 D = 0.10
-
 N = 0.5
 P = .01
 
@@ -16,3 +15,14 @@ if (Number.isNaN(Amount)) {
 } else if (Amount < MIN || Amount > MAX) {
     console.log ("Invalid")
 } else {
+
+  let Amount1 = Amount - Q*Quarter
+  let Quarter = Math.floor(Amount/Q)
+  let Amount2 = Amount - D*Dime
+  let Dime = Math.floor(Amount2/D)
+  let Amount3 = Amount2 - N*Nickel
+  let Nickel = Math.floor(Amount3/N)
+  let Amount4 = Amount3 - P*Penny
+  let Penny = Math.floor(Amount4/P)
+
+console.log (Quarter + "quarters, " + Dime + "dimes, " + Nickel + "nickels, " + Penny + "pennies.");
