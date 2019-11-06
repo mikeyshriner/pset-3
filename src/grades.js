c2c7a2a3297a0ff53128e0f1467eb0705e1af8e6
mikeyshriner <shrinermichael@gmail.com>
2019-11-06 12:58:19 -0500

    hot commit summer

--- src/grades.js
+++ src/grades.js
@@ -2,33 +2,22 @@ const readlineSync = require("readline-sync");
 
 const letterGrade = Number(readlineSync.question("\nEnter a grade: "));
 
-const lowA = 90;
-const highA = 100;
-const lowB = 80;
-const highB = 89;
-const lowC = 70;
-const highC = 79
-const lowD = 60;
-const highD = 69;
-const lowF = 0;
-const highF = 59;
-
 if (letterGrade > 100 || letterGrade < 0) {
       console.log("\nInvalid\n")
 
-} else if (letterGrade >= lowA && letterGrade <= highA) {
+} else if (letterGrade >= 90 && letterGrade <= 100) {
       console.log("\nYou received an A.\n")
 
-} else if (letterGrade >= lowB && letterGrade < lowA) {
+} else if (letterGrade >= 80 && letterGrade < 90) {
       console.log("\nYou received a B.\n")
 
-} else if (letterGrade >= lowC && letterGrade < lowB) {
+} else if (letterGrade >= 70 && letterGrade < 80) {
       console.log("\nYou received a C.\n")
 
-} else if (letterGrade >= lowD && letterGrade < lowC) {
+} else if (letterGrade >= 60 && letterGrade < 70) {
       console.log("\nYou received a D.\n")
 
-} else if (letterGrade >= lowF && letterGrade < lowD) {
+} else if (letterGrade >= 0 && letterGrade < 60) {
       console.log("\nYou received an F.\n")
 
 } else {
